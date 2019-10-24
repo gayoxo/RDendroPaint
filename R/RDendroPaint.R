d2ea2399e1983b0c0aaedec9315fc50491035bdf
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2019-10-24 22:06:33 +0200

    Al fin

--- R/RDendroPaint.R
+++ R/RDendroPaint.R
@@ -1,3 +1,303 @@
+PintaDendro <- function(valoresMatriz,Ekival,RootEle,tablaMatrix) {
+  
+  
+  library(stringi)
+  
+  
+  listaNew=list();
+  
+  if (is.array(valoresMatriz)){
+    subvaloresmatrizCal=valoresMatriz[,1]
+  }else
+    subvaloresmatrizCal=valoresMatriz
+  
+  #Procesa el vector magico
+  
+  for (j in 1:length(subvaloresmatrizCal))
+  {
+    
+    if (as.character(subvaloresmatrizCal[j]) %in% names(listaNew)){
+      listaNew[[as.character(subvaloresmatrizCal[j])]]= c(listaNew[[as.character(subvaloresmatrizCal[j])]],j)
+    }else
+      listaNew[[as.character(subvaloresmatrizCal[j])]]=j
+    
+    
+  }
+  
+  #Procesa la lista con los valores segmentados
+  
+  for (nombrelis in names(listaNew)) {
+    
+    
+    
+    
+    if (length(listaNew[[nombrelis]])<=1)
+    {
+      #print(nombrelis)
+      
+      if (is.array(valoresMatriz)){
+        namesLi=rownames(valoresMatriz)
+      }else
+        namesLi=names(valoresMatriz)
+      
+      # print(namesLi[listaNew[[nombrelis]]])
+      
+      RootEleI<-new ("DendroTree", name = paste("C_", RDendroPaint.var,sep = ""))
+      
+      rownamesvect= namesLi[listaNew[[nombrelis]]]
+      
+      RootEleI@documents<-rownamesvect;
+      
+      RooteVAlue<-procesaListaMatrix(rownamesvect,Ekival,tablaMatrix)
+      if (!is.null(RooteVAlue))
+        RootEleI@values<-RooteVAlue
+      
+      print(RootEleI@name)
+      
+      RDendroPaint.var<<-RDendroPaint.var+1
+      
+      if (is.null(RootEle@sons))
+        RootEle@sons<-RootEleI
+      else
+        RootEle@sons<-c(RootEle@sons,RootEleI)
+      
+    }
+    else{
+      
+      if (is.array(valoresMatriz)){
+        subvaloresmatrizCal=valoresMatriz[listaNew[[nombrelis]],2:ncol(valoresMatriz)]
+      }else
+        subvaloresmatrizCal=valoresMatriz[listaNew[[nombrelis]]]
+      
+      
+      
+      
+      # print(subvaloresmatrizCal)
+      
+      dif=FALSE
+      
+      while (!dif)
+      {
+        
+        if (is.array(subvaloresmatrizCal))
+        {
+          
+          #        print(subvaloresmatrizCal)
+          
+          subvaloresmatrizCal1=subvaloresmatrizCal[,1]
+          subvaloresmatrizCal2=subvaloresmatrizCal[,2]
+          
+          listpar1=list()
+          
+          for (j in 1:length(subvaloresmatrizCal1))
+          {
+            
+            if (as.character(subvaloresmatrizCal1[j]) %in% names(listpar1)){
+              listpar1[[as.character(subvaloresmatrizCal1[j])]]= c(listpar1[[as.character(subvaloresmatrizCal1[j])]],j)
+            }else
+              listpar1[[as.character(subvaloresmatrizCal[j])]]=j
+            
+          }
+          
+          listpar2=list()
+          
+          for (j in 1:length(subvaloresmatrizCal2))
+          {
+            
+            if (as.character(subvaloresmatrizCal2[j]) %in% names(listpar2)){
+              listpar2[[as.character(subvaloresmatrizCal2[j])]]= c(listpar2[[as.character(subvaloresmatrizCal2[j])]],j)
+            }else
+              listpar2[[as.character(subvaloresmatrizCal2[j])]]=j
+            
+          }
+          
+          
+          if (length(names(listpar1))>1&&length(names(listpar1))!=length(names(listpar2)))
+            dif=TRUE
+          else
+            subvaloresmatrizCal=subvaloresmatrizCal[,2:ncol(subvaloresmatrizCal)]
+          
+          #      print(subvaloresmatrizCal)
+          
+        }else
+          dif=TRUE
+      }
+      
+      
+      #    print(subvaloresmatrizCal)
+      
+      if (is.array(subvaloresmatrizCal)||length(subvaloresmatrizCal)>2)
+      {
+        
+        
+        if (is.array(subvaloresmatrizCal))
+        {
+          #Obtener nombres de una Matriz
+          for (i in 1:length(rownames(subvaloresmatrizCal)))
+            if (i==1)
+              rownamesvect=rownames(subvaloresmatrizCal)[1]
+            else
+              rownamesvect=c(rownamesvect,rownames(subvaloresmatrizCal)[i])
+            
+        }else
+        {
+          #Obtener nombre de un Vector
+          for (i in 1:length(names(subvaloresmatrizCal)))
+            if (i==1)
+              rownamesvect<-names(subvaloresmatrizCal)[1]
+            else
+              rownamesvect<-c(rownamesvect,names(subvaloresmatrizCal)[i])
+        }
+        
+        
+        
+        RootEleI<-new ("DendroTree", name = paste("C_", RDendroPaint.var,sep = ""))
+        
+        RootEleI@documents<-rownamesvect;
+        
+        RooteVAlue<-procesaListaMatrix(rownamesvect,Ekival,tablaMatrix)
+        if (!is.null(RooteVAlue))
+          RootEleI@values<-RooteVAlue
+        
+        print(RootEleI@name)
+        
+        RDendroPaint.var<<-RDendroPaint.var+1
+        
+        RootEleI<-PintaDendro(subvaloresmatrizCal,Ekival,RootEleI,tablaMatrix)
+        
+        if (is.null(RootEle@sons))
+          RootEle@sons<-RootEleI
+        else
+          RootEle@sons<-c(RootEle@sons,RootEleI)
+        
+        
+
+       
+        
+  #      print(RootEleI@sons)
+      }
+      else
+      {
+        
+        for (i in 1:length(names(subvaloresmatrizCal)))
+          if (i==1)
+            rownamesvect<-names(subvaloresmatrizCal)[1]
+          else
+            rownamesvect<-c(rownamesvect,names(subvaloresmatrizCal)[i])
+          
+          
+          RootEleI<-new ("DendroTree", name = paste("C_", RDendroPaint.var,sep = ""))
+          
+          RootEleI@documents<-rownamesvect;
+          
+          RooteVAlue<-procesaListaMatrix(rownamesvect,Ekival,tablaMatrix)
+          if (!is.null(RooteVAlue))
+            RootEleI@values<-RooteVAlue
+          
+          print(RootEleI@name)
+          
+          RDendroPaint.var<<-RDendroPaint.var+1
+          
+         
+          
+          
+
+          
+          for (nombresTa in names(subvaloresmatrizCal)) {
+            
+            
+            RootEleI2<-new ("DendroTree", name = paste("C_", RDendroPaint.var,sep = ""))
+            
+            
+            RootEleI2@documents<-nombresTa;
+            
+            RooteVAlue<-procesaListaMatrix(nombresTa,Ekival,tablaMatrix)
+            if (!is.null(RooteVAlue))
+              RootEleI2@values<-RooteVAlue
+            
+            print(RootEleI2@name)
+            
+            RDendroPaint.var<<-RDendroPaint.var+1
+            
+            if (is.null(RootEleI@sons))
+              RootEleI@sons<-RootEleI2
+            else
+              RootEleI@sons<-c(RootEleI@sons,RootEleI2)
+            
+          }
+          
+          
+          
+          if (is.null(RootEle@sons))
+            RootEle@sons<-RootEleI
+          else
+            RootEle@sons<-c(RootEle@sons,RootEleI)
+      }
+      
+    }
+  }
+  
+  return (RootEle)
+  
+}
+
+
+
+procesaListaMatrix <- function (entrada,Ekival,tablaMatrix)
+{
+  
+  #print(Ekival)
+  #print(entrada)
+  
+  validos=c();
+  if (is.vector(entrada))
+  {
+    
+    for (i in 1:length(entrada))
+    {
+      validos=c(validos,Ekival[[entrada[i]]])
+    }
+    
+  }
+  else
+    validos=c(validos,Ekival[[entrada]])
+  
+  #  print("SI")
+  #  print(validos)
+  
+  subma=tablaMatrix[validos,]
+  #  print(subma)
+  
+  salida=c()
+  if (is.array(subma))
+  {
+    
+    for (i in 1:ncol(subma))
+    {
+      foundZero=FALSE
+      for(j in 1:nrow(subma))
+        if (subma[j,i]==0)
+          foundZero=TRUE
+        
+        if (!foundZero)
+          salida=c(salida,colnames(subma)[i])
+    }
+  }
+  else
+  {
+    for (i in 1:length(subma)){
+      if (subma[i]==1)
+        salida=c(salida,names(subma)[i])
+      
+    }
+  }
+
+  
+  return(salida)
+}
+
+
+
 #'@title RDendroPaint
 #'@description Paint in console the dondrogram and return a list with non empty cross elements group
 #'@param valoresMatriz Tree Matrix with all grups generatod by, normally from 2 to limit-1 \"cutree(hclust_avg, k =2:(length(hclust_avg$order)-1))\"
@@ -5,12 +305,119 @@
 #'@return Object DendroTree 
 RDendroPaint <- function(valoresMatriz,tablaMatrix) {
   
-  setClass("DendroTree", slots=list(name="character", sons="list", documents="list", values="list"))
+  setClass("DendroTree", slots=list(name="character", sons="list", documents="character", values="character"))
+  
+  Ekival=list();
+  
+  for (i in 1:length(rownames(tablaMatrix)))
+    Ekival[[rownames(tablaMatrix)[i]]]=i
   
   
+  
+  RDendroPaint.var<<-0
+  
+  rownamesvect=c()
+  
+  if (is.array(valoresMatriz))
+  {
+    rownamesvect=rownames(valoresMatriz)
+      
+  }else
+  {
+    #Obtener nombre de un Vector
+    rownamesvect=names(valoresMatriz)
+  }
+  
+  RootEle<-new ("DendroTree", name = paste("C_", RDendroPaint.var,sep = ""))
+  
+  RootEle@documents<-c(rownamesvect);
+  
+  RooteVAlue<-procesaListaMatrix(rownamesvect,Ekival,tablaMatrix)
+  if (!is.null(RooteVAlue))
+    RootEle@values<-RooteVAlue
+  
+  print(RootEle@name)
+  
+  RDendroPaint.var<<-RDendroPaint.var+1
+  
+  RootEle<-PintaDendro(valoresMatriz,Ekival,RootEle,tablaMatrix)
+  
+  return(RootEle)
   
 }
 
+pintaRDendroPaint<- function(ListaSal){
+  
+  level=0
+  pintaRDendroPaintLevel(ListaSal,level)
+
+}
+
+pintaRDendroPaintLevel<- function(ListaSal,level){
+  
+  
+  
+  
+  
+  if (is.vector(ListaSal@documents))
+  {
+    for (i in 1:length(ListaSal@documents)){
+      if (i==1)
+        rownamesvect=ListaSal@documents[i]
+      else
+        rownamesvect=paste(rownamesvect,ListaSal@documents[i],sep = ",")
+    }
+  }
+  else
+    rownamesvect=ListaSal@documents
+  
+  
+  base=""
+  for(i in 0:level)
+    base<- paste(base,"--",sep = "")
+  
+  #setClass("DendroTree", slots=list(name="character", sons="list", documents="character", values="character"))
+  print(paste(base,"name: ", ListaSal@name,sep = ""))
+  
+  if (is.vector(ListaSal@documents))
+  {
+    for (i in 1:length(ListaSal@documents)){
+      if (i==1)
+        rownamesvect=ListaSal@documents[i]
+      else
+        rownamesvect=paste(rownamesvect,ListaSal@documents[i],sep = ",")
+    }
+  }
+  else
+    rownamesvect=ListaSal@documents
+  
+  print(paste(base,"Documentos: ",rownamesvect ,sep = ""))
+  
+  if (is.vector( ListaSal@values))
+  {
+    if (length( ListaSal@values)>0)
+    {
+    for (i in 1:length( ListaSal@values)){
+      if (i==1)
+        rownamesvect=ListaSal@values[i]
+      else
+        rownamesvect=paste(rownamesvect,ListaSal@values[i],sep = ",")
+    }
+    }else
+      rownamesvect=""
+    
+  }
+  else
+    rownamesvect=ListaSal@values
+  
+  print(paste(base,"Values: ",rownamesvect,sep = ""))
+  
+  for(doc in ListaSal@sons)
+  {
+    pintaRDendroPaintLevel(doc,level+1)
+  }
+    
+}
 
 #Carga de librerias
 library(LSAfun)
@@ -195,4 +602,5 @@ valoresDendro = cutree(hclust_avg, k =2:(length(hclust_avg$order)-1))
 
 ListaSal=RDendroPaint(valoresDendro,t(TDM))
 
-print(names(ListaSal))
+pintaRDendroPaint(ListaSal)
+
